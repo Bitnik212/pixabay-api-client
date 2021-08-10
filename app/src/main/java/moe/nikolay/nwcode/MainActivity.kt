c15d5003ceff2a42fbc5ab8a0e630002f6a98be0
bitnik212 <nnv@bitapp.me>
2021-08-11 01:54:28 +0300

    fix/refresh when click on current tab

--- app/src/main/java/moe/nikolay/nwcode/MainActivity.kt
+++ app/src/main/java/moe/nikolay/nwcode/MainActivity.kt
@@ -6,12 +6,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import moe.nikolay.nwcode.databinding.ActivityMainBinding
-import moe.nikolay.nwcode.repository.api.pixaby.PixabayAPI
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -20,19 +16,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-
         navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val bottomNavigationView: BottomNavigationView = binding.navView
+        bottomNavigationView.setupWithNavController(navController)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_settings))
+
+        //fix refresh when click on current tab
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            if(item.itemId != bottomNavigationView.selectedItemId)
+                NavigationUI.onNavDestinationSelected(item, navController)
+            true
+        }
+
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
     }
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
